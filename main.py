1049112687663b3882301c4acdab6003abfce12a
Bob Wei <bobqywei@gmail.com>
2020-04-11 19:06:20 -0400

    Updated a2c and env

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import yaml
 import os
 import argparse
 import torch
+import gym
 from a2c import A2C
 from tensorboardX import SummaryWriter
 
@@ -10,6 +11,10 @@ parser = argparse.ArgumentParser(description="Curiosity-driven A2C")
 parser.add_argument('--config', default='configs/main.yaml')
 
 def main():
+    
+    gym_env = gym.make(config['task'])
+    gym_env.reset()
+    
     args = parser.parse_args()
 
     with open(args.config) as f:
@@ -17,5 +22,5 @@ def main():
 
     device = torch.device('cuda') if config['use_gpu'] else torch.device('cpu')
 
-    a2c = A2C(config, env, device)
+    a2c = A2C(config, gym_env, device)
     A2C.train()
