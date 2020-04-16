f54255546e2caeee23a7f030d3c724ecb3ed74d8
Bob Wei <bobqywei@gmail.com>
2020-04-16 19:59:39 -0400

    update figures

--- plot.py
+++ plot.py
@@ -14,7 +14,7 @@ def smooth(scalars, weight):
     last = scalars[0]
     smoothed = list()
     for point in scalars:
-        smoothed_val = last * weight + (1 - weight) * point 
+        smoothed_val = last * weight + (1 - weight) * point
         smoothed.append(smoothed_val)
         last = smoothed_val
     return smoothed
@@ -24,8 +24,10 @@ col_names = ['Step', 'Value']
 data = []
 for path in args.paths:
     df = pd.read_csv(path, names=col_names, skipinitialspace=True)
-    data.append((df.Step.values[1:].astype(int), df.Value.values[1:].astype(float)))
+    data.append([df.Step.values[1:].astype(int), df.Value.values[1:].astype(float)])
 
+# data[-1][1] = data[-1][1][:-45]
+# data[-1][0] = data[-1][0][:-45]
 colours = ['r', 'y', 'c']
 labels = ['A2C', 'A2C + Entropy', 'A2C + Entropy + Curiosity']
 i = 0
@@ -33,6 +35,8 @@ for d in data:
     plt.plot(d[0], d[1], alpha=0.2, color=colours[i])
     plt.plot(d[0], smooth(d[1], 0.95), color=colours[i], label=labels[i])
     i = (i+1) % 3
+
+# plt.ylim(0, 1800)
 plt.legend()
 plt.xlabel('Training Update Steps')
 plt.ylabel('Extrinsic Reward per Episode')
